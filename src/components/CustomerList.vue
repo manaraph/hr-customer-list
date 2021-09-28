2290dc64d249797f435a42553b6e9051d5ab59fb
manaraph <manaraph225@gmail.com>
2021-09-29 00:12:42 +0100

    Progress with showing customer list

--- src/components/CustomerList.vue
+++ src/components/CustomerList.vue
@@ -1,29 +1,64 @@
 <template>
   <div class="mt-75 layout-column justify-content-center align-items-center">
     <section class="layout-row align-items-center justify-content-center">
-      <input type="text" class="large" placeholder="Name"
-             data-testid="app-input"/>
-      <button type="submit"
-              class="ml-30"
-              data-testid="submit-button">
+      <input
+        v-model="customerName"
+        type="text"
+        class="large"
+        placeholder="Name"
+        data-testid="app-input"
+      />
+      <button
+        type="submit"
+        class="ml-30"
+        data-testid="submit-button"
+        @click="addCustomer"
+      >
         Add Customer
       </button>
     </section>
-    <ul class="styled mt-50" data-testid="customer-list">
-<!--      Use this section for rendering each customer as it contains the requisite test-id required for tests to pass-->
-<!--      <li class="slide-up-fade-in"-->
-<!--          :data-testid="`list-item${index}`" :key="`list-item${index}`">-->
-<!--      </li>-->
+    <ul
+      v-if="customerList.length"
+      class="styled mt-50"
+      data-testid="customer-list"
+    >
+      <!--      Use this section for rendering each customer as it contains the requisite test-id required for tests to pass-->
+      <li
+        v-for="(customer, index) in customerList"
+        class="slide-up-fade-in"
+        :data-testid="`list-item${index}`"
+        :key="`list-item${index}`"
+      >
+        {{ customer }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "CustomerList"
-}
+  name: "CustomerList",
+  data() {
+    return {
+      customerName: "",
+      customerList: [],
+    };
+  },
+  mounted() {
+    this.customerName = "";
+  },
+  methods: {
+    addCustomer() {
+      if (this.customerName.trim() === "") {
+        this.customerName = "";
+        return false;
+      }
+      this.customerList.push(this.customerName);
+      this.customerName = "";
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
